be5f76aeaae788574cfbe1cd8fe7005c013c33ba
Adrian Silva <adrian@bvox.net>
2010-11-02 16:37:42 +0100

    open ntp port on xenserver

---
+++ xenserver-blend.rb
@@ -0,0 +1,20 @@
+meta :iptables_port do
+  accepts_list_for :port_name
+  accepts_list_for :proto
+  template {
+    met? {
+      iptables_rules = shell 'iptables --list'
+      iptables_rules =~ /^ACCEPT.*#{proto} dpt:#{port_name}/
+    }
+    meet {
+      shell "iptables -A RH-Firewall-1-INPUT -m state --state NEW " \
+            "-m #{proto} -p #{proto} --dport #{port_name} -j ACCEPT"
+      shell '/sbin/service iptables save'
+    }
+  }
+end
+
+dep 'ntp.iptables_port' do
+  port_name 'ntp'
+  proto 'udp'
+end
